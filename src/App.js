997f04109e5b8d869eb1efbbc3a905df017a3744
Nhuan <nhuan.hoangduc@outlook.com>
2018-03-14 23:18:59 +0700

    Add bootstrap 4.0 cdn to index.html

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
 import React, { Component } from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+
+import PgUserCreate from './pages/PgUserCreate';
 
 class App extends Component {
     render() {
         return (
             <div>
-                React app
+                {/* Header */}
+                <header>
+                    <nav className="navbar navbar-light bg-light">
+                        <a className="navbar-brand" href="/">Navbar</a>
+                    </nav>
+                </header>
+
+
+                {/* Body */}
+                <BrowserRouter>
+                    {/* here's a div */}
+                    <div>
+                        {/* here's a Route */}
+                        <Route path="/" component={PgUserCreate} />
+                    </div>
+                </BrowserRouter>
             </div>
         );
     }
