bc628d406b8820ec5159c83997720477505df74b
Nhuan <nhuan.hoangduc@outlook.com>
2018-03-15 00:57:44 +0700

    Page - User Create - Complete

--- src/pages/PgUserCreate.js
+++ src/pages/PgUserCreate.js
@@ -1,13 +1,38 @@
 import React, { Component } from 'react';
 
+import UserApi from '../api/user';
+import UserEditForm from '../forms/UserEditForm';
+
+
 class PgUserCreate extends Component {
+
+    componentDidMount() {
+        document.title = "Create user";
+    }
+
+
+    handleFormSubmit(user) {
+        return async (event) => {
+            event.preventDefault();
+
+            if (!user.name) {
+                return;
+            }
+
+            await UserApi.createUser(user);
+            window.location = '/';
+        };
+    }
+
+
     render() {
         return (
             <div>
-                Create form
+                <UserEditForm onSubmitForm={this.handleFormSubmit.bind(this)} />
             </div>
         );
     }
 }
+
 
 export default PgUserCreate;
