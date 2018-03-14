3430b8d544c8f0e1db5fece8abb8460e22ccb06f
Nhuan <nhuan.hoangduc@outlook.com>
2018-03-15 00:58:00 +0700

    Page - User Update - Complete

--- src/pages/PgUserUpdate.js
+++ src/pages/PgUserUpdate.js
@@ -1,13 +1,60 @@
 import React, { Component } from 'react';
 
-class PgUserUpdate extends Component {
+import UserApi from '../api/user';
+import UserEditForm from '../forms/UserEditForm';
+
+
+class PgUserCreate extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            _id: props.match.params.id
+        };
+    }
+
+
+    componentDidMount() {
+        document.title = "Update user";
+        this.getUser();
+    }
+
+
+    async getUser() {
+        // Get all users
+        const user = await UserApi.getUser(this.state._id);
+        this.setState({ ...this.state, ...user });
+    }
+
+
+    handleFormSubmit(user) {
+        return async (event) => {
+            event.preventDefault();
+
+            if (!user.name) {
+                return;
+            }
+
+            await UserApi.updateUser(this.state._id, user);
+            window.location = '/';
+        };
+    }
+
+
     render() {
+        const user = this.state;
+        
+        if (!user.name) {
+            return <div>Loading...</div>
+        }
+
         return (
             <div>
-                Update form
+                <UserEditForm user={user} onSubmitForm={this.handleFormSubmit.bind(this)} />
             </div>
         );
     }
 }
 
-export default PgUserUpdate;
+
+export default PgUserCreate;
