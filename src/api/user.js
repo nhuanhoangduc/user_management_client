4cfcc3fced3390624eedb3597d5ea0bb3b9bb3e7
Nhuan <nhuan.hoangduc@outlook.com>
2018-03-15 00:56:47 +0700

    Api - User - Add get user, add and update user services

--- src/api/user.js
+++ src/api/user.js
@@ -13,6 +13,38 @@ export const UserApi = {
         }
     },
 
+
+    getUser: async function (userId) {
+        try {
+            const response = await axios.get(Configs.API_URL + Configs.USER_API_ENDPOINT + '/' + userId);
+            const user = response.data;
+            return user;
+        } catch (error) {
+
+        }
+    },
+
+
+    createUser: async function (user) {
+        try {
+            const response = await axios.post(Configs.API_URL + Configs.USER_API_ENDPOINT, user);
+            return response;
+        } catch (error) {
+
+        }
+    },
+
+
+    updateUser: async function (userId, user) {
+        try {
+            const response = await axios.put(Configs.API_URL + Configs.USER_API_ENDPOINT + '/' + userId, user);
+            return response;
+        } catch (error) {
+
+        }
+    },
+
+
     deleteUser: async function (userId) {
         try {
             const response = await axios.delete(Configs.API_URL + Configs.USER_API_ENDPOINT + '/' + userId);
